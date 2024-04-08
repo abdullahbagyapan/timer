0727756c72dfd49d4e45a54937030c1c7345a27d
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-08 17:06:37 +0300

    feat::add header file:CLOCK
    -Define function
    --CLOCK_Init()

---
+++ src/clock/CLOCK.h
@@ -0,0 +1,29 @@
+/*
+ @module: CLOCK
+ @author: Abdullah Bagyapan
+ @date:   08/04/2024
+*/
+
+
+/*================================== Libraries ==================================*/
+
+
+// Standart libraries
+#include <stdint.h>
+
+
+/*================================== Functions ==================================*/
+
+
+/*
+ @brief:  Initialize CLOCK system
+
+ @author  Abdullah Bagyapan
+
+ @date    08/04/2024
+
+ @param   None
+
+ @return  None
+*/
+void CLOCK_Init();
