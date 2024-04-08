d27206abcf885bff73b1bb774c03ff12c6910d86
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-08 22:14:10 +0300

    add::timeout:CLOCK

--- src/clock/CLOCK.h
+++ src/clock/CLOCK.h
@@ -33,3 +33,19 @@
  @return  None
 */
 void CLOCK_Init();
+
+
+
+
+/*
+ @brief:  Return timer is timeout or not
+
+ @author  Abdullah Bagyapan
+
+ @date    08/04/2024
+
+ @param   None
+
+ @return  uint8_t, if it is timeout return 1, otherwise return 0
+*/
+uint8_t CLOCK_Timeout();
